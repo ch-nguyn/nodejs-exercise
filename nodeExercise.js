3c50d59953a500ad65e4323cb336b81828d3eca8
Chien <cn555444@gmail.com>
2023-12-18 15:00:24 +0700

    update code clean

--- nodeExercise.js
+++ nodeExercise.js
@@ -33,10 +33,12 @@ const getPostWithCommentById = async (id) => {
 (async () => {
   try {
     // 2. Get data from all users from API above. You will get a list of 10 users.
-    const users = await getData("users");
-
     // 3. Get all the posts and comments from the API. Map the data with the users array.
-    const [posts, comments] = await getLotsOfData(["posts", "comments"]);
+    const [posts, comments, users] = await getLotsOfData([
+      "posts",
+      "comments",
+      "users",
+    ]);
 
     const userData = users.map((user) => {
       user.posts = posts.filter((post) => post.userId === user.id);
@@ -45,17 +47,14 @@ const getPostWithCommentById = async (id) => {
       );
       return user;
     });
-    console.log("3. ", userData);
+    console.log("2, 3. ", userData);
 
     // 4. Filter only users with more than 3 comments.
-    const usersMoreThan3Cmts = [...users].filter(
-      (user) => user.comments.length > 3
-    );
+    const usersMoreThan3Cmts = users.filter((user) => user.comments.length > 3);
     console.log("4. ", usersMoreThan3Cmts);
 
     // 5. Reformat the data with the count of comments and posts
-    const dataWithCount = [...users];
-    const result = dataWithCount.map((user) => {
+    const result = users.map((user) => {
       user.commentsCount = user.comments.length;
       user.postsCount = user.posts.length;
       return user;
@@ -72,12 +71,11 @@ const getPostWithCommentById = async (id) => {
     console.log("6.2. ", userWithMostPosts);
 
     // 7. Sort the list of users by the postsCount value descending?
-    const dataWithSort = [...dataWithCount];
+    const dataWithSort = [...users];
     dataWithSort.sort((a, b) => b.postsCount - a.postsCount);
     console.log("7. ", dataWithSort);
 
     // 8. Get the post with ID of 1 via API request, at the same time get comments for post ID of 1 via another API request
-
     const [post1, commentsOfPost1] = await getPostWithCommentById("1");
 
     const post = {
